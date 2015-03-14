0845a0e3dc14ba9c9cae51ec54fe914b028e4858
jenkins <jenkins@teamw.in>
2015-03-14 19:53:46 -0400

    f2fs updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_PREBUILT_KERNEL := device/samsung/hltecan/kernAl
 # Use this flag if the board has a ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # TWRP specific build flags
 DEVICE_RESOLUTION := 1080x1920

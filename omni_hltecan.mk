437bb1406b07712f12d3e55c66acdd4d74fb27d9
James Christopher Adduono <jc@adduono.com>
2016-02-07 18:17:25 -0500

    Initialize repo

---
+++ omni_hltecan.mk
@@ -0,0 +1,15 @@
+# Release name
+PRODUCT_RELEASE_NAME := hltecan
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, build/target/product/full.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hltecan
+PRODUCT_NAME := omni_hltecan
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-N900W8
+PRODUCT_MANUFACTURER := samsung
